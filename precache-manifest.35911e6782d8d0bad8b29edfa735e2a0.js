fc6b218279be20a9d00a30bb516bcb047d998791
jesus <jesusiniesta@fastmail.com>
2019-04-21 22:49:17 +0200

    Updates

--- precache-manifest.35911e6782d8d0bad8b29edfa735e2a0.js
+++ precache-manifest.35911e6782d8d0bad8b29edfa735e2a0.js
@@ -1,22 +1,22 @@
 self.__precacheManifest = [
   {
-    "revision": "7c83905332406d00d0ca",
+    "revision": "ea29691161eb428324d0",
     "url": "/jkurei/angry-metal-playlists/static/css/main.32788b0b.chunk.css"
   },
   {
-    "revision": "7c83905332406d00d0ca",
-    "url": "/jkurei/angry-metal-playlists/static/js/main.13fc4421.chunk.js"
+    "revision": "ea29691161eb428324d0",
+    "url": "/jkurei/angry-metal-playlists/static/js/main.2d4d9d66.chunk.js"
   },
   {
     "revision": "7ac0351336df5cd0abd1",
     "url": "/jkurei/angry-metal-playlists/static/js/runtime~main.d3482ab1.js"
   },
   {
-    "revision": "c3f9408f9a9e2712b8f5",
-    "url": "/jkurei/angry-metal-playlists/static/js/2.6269b572.chunk.js"
+    "revision": "cf9df6c309d80b571a26",
+    "url": "/jkurei/angry-metal-playlists/static/js/2.4267ee5e.chunk.js"
   },
   {
-    "revision": "fbad2951bf1712bf7fd304688d2b8d8b",
+    "revision": "438c4922471f09fab3a8d9c09e1f5ee8",
     "url": "/jkurei/angry-metal-playlists/index.html"
   }
 ];
